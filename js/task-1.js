7fa09a3e74b5132989555a5818fd188045c205ff
Deakalka <fedorushun.borya@gmail.com>
2023-12-20 04:44:13 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,12 @@
-function slugify(title){
-    const lowerTitle = title.toLowerCase();
-    let slug = '';
-    for(let i = 0; i < lowerTitle.length; i++){
-        const char = lowerTitle[i];
-        if(char === " "){
-            slug += "-";
-        } else {
-            slug += char;
-        }  
+function isEnoughCapacity(products, containerSize) {
+    let count = 0;
+    for (product in products){
+        count += products[product];
     }
-    return slug;
+    return count <= containerSize;
 }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log( isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log( isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log( isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+console.log( isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
